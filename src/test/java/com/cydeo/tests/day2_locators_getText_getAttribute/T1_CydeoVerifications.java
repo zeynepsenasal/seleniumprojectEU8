8a57c27e25d5c7d5cd60406899dcb45fe6b6e0ca
Zeynep Sena <zeynepssaltik@gmail.com>
2022-05-06 21:47:39 +0300

    day2 CydeoVerifications

--- src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T1_CydeoVerifications.java
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T1_CydeoVerifications.java
@@ -1,4 +1,25 @@
 package com.cydeo.tests.day2_locators_getText_getAttribute;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
 public class T1_CydeoVerifications {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver= new ChromeDriver();
+        driver.manage().window().maximize();
+
+
+
+
+
+
+
+
+
+    }
 }
