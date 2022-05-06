9d7d0faa771b4ff5f5eb3e58409769467bc21306
Zeynep Sena <zeynepssaltik@gmail.com>
2022-05-06 21:54:08 +0300

    day2 CydeoVerifications

--- src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T1_CydeoVerifications.java
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T1_CydeoVerifications.java
@@ -13,9 +13,27 @@ public class T1_CydeoVerifications {
         WebDriver driver= new ChromeDriver();
         driver.manage().window().maximize();
 
+        //go to the page
+        driver.navigate().to("https://practise.cydeo.com");
 
+        String expectedURL= "cydeo";
+        String actualURL=driver.getCurrentUrl();
 
+        if(actualURL.contains(actualURL))
+        {
+            System.out.println("URL Test PASSED");
+        }else{
+            System.out.println("URL Test FAİLED");
+        }
 
+        String expectedTitle= "Practise";
+        String actualTitle = driver.getTitle();
+
+        if(expectedTitle.equals(actualTitle)){
+            System.out.println("Title verification PASSED");
+        }else{
+            System.out.println("Title verification PASSED");
+        }
 
 
 
