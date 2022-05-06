066099682a6df244c091fe97cc988a0bf94d4081
Zeynep Sena <zeynepssaltik@gmail.com>
2022-05-06 21:48:16 +0300

    Revert "day2 Yahoo Task"
    This reverts commit 8f404b7d01922aa73cc17ed1ab723f3da702632e.

--- src/test/java/com/cydeo/tests/day1_selenium_intro/Task1_YahooVerification.java
+++ src/test/java/com/cydeo/tests/day1_selenium_intro/Task1_YahooVerification.java
@@ -1,29 +1,10 @@
 package com.cydeo.tests.day1_selenium_intro;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
 public class Task1_YahooVerification {
     public static void main(String[] args) {
 
-        WebDriverManager.chromedriver().setup();
 
-        WebDriver driver= new ChromeDriver();
-        driver.manage().window().maximize();
 
-        driver.get("https://www.yahoo.com");
-        //expected: Yahoo | Mail, Weather, Search, Politics, News, Finance, Sports & Videos
-
-        String expectedTitle= "Yahoo | Mail, Weather, Search, Politics, News, Finance, Sports & Videos";
-
-        String actualTitle = driver.getTitle();
-
-        if(actualTitle.equals(expectedTitle)){
-            System.out.println("Title is expected. Verification PASSED");
-        }else{
-            System.out.println("Title is not expected. Verification FAILED");
-        }
 
 
 
