982b9230366afbf304bbb935b109e33ee75f3ca7
Zeynep Sena <zeynepssaltik@gmail.com>
2022-05-31 09:45:45 +0300

    testbasepractice

--- src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T3_CRM_Login.java
+++ src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T3_CRM_Login.java
@@ -2,21 +2,11 @@ package com.cydeo.tests.day7_webtables_utilities_javafaker;
 
 import com.cydeo.Utilities.BrowserUtils;
 import com.cydeo.Utilities.CRM_Utilities;
-import com.cydeo.Utilities.WebDriverFactory;
-import org.openqa.selenium.WebDriver;
-import org.testng.annotations.BeforeMethod;
+import com.cydeo.tests.base.TestBase;
 import org.testng.annotations.Test;
 
-import java.util.concurrent.TimeUnit;
+public class T3_CRM_Login extends TestBase {
 
-public class T3_CRM_Login {
-    WebDriver driver;
-    @BeforeMethod
-    public void setUpMethod(){
-        driver = WebDriverFactory.getDriver("chrome");
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-    }
     @Test
     public void crm_login_test(){
         driver.get("https://login1.nextbasecrm.com");
